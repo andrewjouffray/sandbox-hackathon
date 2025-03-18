f8bc1ab04d72cecc3f45da6f7feb6b8bb5a2feda
jtjolly <jtjolly@gmail.com>
2025-03-18 09:33:27 -0600

    basic openAI endpoint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,9 +6,11 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 from app.controllers.test_controller import test_bp
+from app.controllers.ai_controller import ai_bp
 from app import models
 
 app.register_blueprint(test_bp)
+app.register_blueprint(ai_bp)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
